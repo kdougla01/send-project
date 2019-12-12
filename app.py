880e9b49f71c3c9dbfe0e756396ad3262d2ae2ff
Mr Douglass <k.dougla01@pudseygrangefield.co.uk>
2019-12-12 12:58:31 +0000

    v0.2
    First functioning version of Send-Project.
    Includes Adding Students, Viewing and more info

--- app.py
+++ app.py
@@ -1,49 +1,60 @@
 from flask import Flask, render_template, redirect
-from forms import addMovieForm, searchMovieForm
+from forms import addStudentForm, searchStudentForm
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
 app.secret_key = "secret key"
-app.config["MONGO_URI"] = "mongodb://localhost:27017/user-movies"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/send-project-data"
 mongo = PyMongo(app)
 
 @app.route('/')
-def userMovies():
-    favMovies = mongo.db.userMovies.find()
+def stuDetails():
+    stuList = mongo.db.studentList.find()
     #return favMovies
-    return render_template('userMovies.html', favMovies=favMovies)
+    return render_template('viewStudents.html', stuList=stuList)
 
-@app.route('/addMovie', methods=['GET','POST'])
-def addMovie():
-    form = addMovieForm()
+@app.route('/addStudent', methods=['GET','POST'])
+def addStudent():
+    form = addStudentForm()
 
     if form.validate_on_submit():
-        form_data = {'_id': form.movieId.data, 
-                    'title': form.movieTitle.data, 
-                    'genre': form.movieGenre.data, 
-                    'rating': form.movieRating.data, 
-                    'released': form.movieReleased.data}
-        addToDb = mongo.db.userMovies.insert(form_data)
+        form_data = {'_id': form.stuId.data, 
+                    'stuFirstName': form.stuFirstName.data, 
+                    'stuLastName': form.stuLastName.data, 
+                    'stuGender': form.stuGender.data, 
+                    'stuYearGroup': form.stuYearGroup.data,
+                    'stuFormClass': form.stuFormClass.data,
+                    'stuSenStatus': form.stuSenStatus.data,
+                    'stuBarrier': form.stuBarrier.data,
+                    'stuReadingAge': form.stuReadingAge.data,
+                    'stuFullDescription': form.stuFullDescription.data}
+        addToDb = mongo.db.studentList.insert(form_data)
         return redirect('/')
-    return render_template('addMovie.html', form=form)
-
+    return render_template('addStudent.html', form=form)
+'''
 @app.route('/searchMovie', methods=['GET','POST'])
 def searchMovie():
-    form = searchMovieForm()
+    form = searchStudentForm()
 
     if form.validate_on_submit():
         result = mongo.db.userMovies.find({'title':{'$regex':'^'+form.movieTitle.data}})
         return render_template('userMovies.html', favMovies=result)
     return render_template('searchMovie.html', form=form)
-    
+'''  
 # Added delete route from movie-api
 @app.route('/delete/<id>', methods=['POST'])
 def delete_movie(id):
-    mongo.db.userMovies.delete_one({'_id': id})
+    mongo.db.studentList.delete_one({'_id': id})
     return redirect('/')
 
+@app.route('/info/<id>', methods=['GET','POST'])
+def stuMoreInfo(id):
+    moreInfo = mongo.db.studentList.find({'_id': id})
+
+    return render_template('stuMoreDetails.html', moreInfo = moreInfo)
+'''
 @app.route('/watched/<id>', methods=['POST'])
 def watched_movie(id):
     mongo.db.userMovies.update({'_id':id},{'$set':{'watched': 'true'}})
@@ -53,7 +64,7 @@ def watched_movie(id):
 def unwatch_movie(id):
     mongo.db.userMovies.update({'_id':id},{'$set':{'watched': 'false'}})
     return redirect('/')
-
+'''
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='127.0.0.1')
--- forms.py
+++ forms.py
@@ -1,13 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, TextField
 from wtforms.validators import DataRequired
 
-class addMovieForm(FlaskForm):
-	movieId = StringField('movieId', validators=[DataRequired()])
-	movieTitle = StringField('movieTitle', validators=[DataRequired()])
-	movieGenre = StringField('movieGenre', validators=[DataRequired()])
-	movieRating = StringField('movieRating', validators=[DataRequired()])
-	movieReleased = IntegerField('movieReleased', validators=[DataRequired()])
+class addStudentForm(FlaskForm):
+	stuId = StringField('Id', validators=[DataRequired()])
+	stuFirstName = StringField('First Name', validators=[DataRequired()])
+	stuLastName = StringField('Last Name', validators=[DataRequired()])
+	stuGender = StringField('Gender', validators=[DataRequired()])
+	stuYearGroup = IntegerField('Year Group', validators=[DataRequired()])
+	stuFormClass = StringField('Form Class', validators=[DataRequired()])
+	stuSenStatus = StringField('Sen Status', validators=[DataRequired()])
+	stuBarrier = StringField('Barrier', validators=[DataRequired()])
+	stuReadingAge = StringField('Reading Age', validators=[DataRequired()])
+	stuFullDescription = TextField('Full Description', validators=[DataRequired()])
 
-class searchMovieForm(FlaskForm):
-	movieTitle = StringField('movieTitle')
+
+class searchStudentForm(FlaskForm):
+	stuFirstName = StringField('stuFirstName')
